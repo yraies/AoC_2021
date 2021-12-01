56f98902525d61f6cc2b5e886c5c93e9df3c6f51
Yasin Raies <raies@posteo.de>
2021-12-01 01:16:08 +0100

    Prepare for AoC 2021

---
+++ src/dayn.rs
@@ -0,0 +1,16 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+//#[aoc_generator(day)]
+pub fn generate_vm(input: &str) -> usize {
+    0
+}
+
+//#[aoc(day, part1)]
+pub fn day_part1(input: &usize) -> usize {
+    *input
+}
+
+//#[aoc(day, part2)]
+pub fn day_part2(input: &usize) -> usize {
+    *input
+}
